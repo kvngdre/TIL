579542f003c112a94eac292bb07fcc3dc01aa8cc
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2021-12-03 00:50:14 +0100

    made all associations

--- Strftime.py
+++ Strftime.py
@@ -2,10 +2,9 @@
 
 from time import strftime, gmtime, localtime
 
-created = "Created Thurs 02 Dec 2021 22:43"
-track = "Amaarae - SAD, U BROKE MY HEART"
 
-a = strftime("%H:%M:%S", localtime())  # So to remember this I try to memorize that time in 24h format = ALL UPPERCASE.
+a = strftime("%H:%M:%S", localtime())
+# So to remember this I try to memorize that time in 24h format = ALL UPPERCASE.
 print(a)
 
 s = strftime("%a, %d %b %Y %H:%M:%S + 1010", gmtime())
@@ -16,18 +15,47 @@ s = strftime("%a, %d %b %Y %H:%M:%S + 1010", gmtime())
 # and %Y=full numeric year......(Number) -> {1010},
 # You gerrit? if you don't gerrit forget about it. 😁
 # it works the same for localtime() function
-# print(strftime("%a, %d %b %Y %H:%M:%S + 1010", localtime()))
 print("Example 1:", s)
 
 """Full Day Name_ seems to print a short date with all numbers m/d/y full month name year per usual."""
-s1 = strftime("%A, %D %B %Y %H:%M:%S + 0000", gmtime())
-print("Example 2: ", s1)
+s1 = strftime("%A, %D %B %Y %H:%M:%S + 0101", localtime())
+# For this I tried to relate it to the previous 1010 so,
+# %A=full name of day of the week[THURSDAY].......(Uletter) 0
+# %D=month starts full short / date all numbers....(Number) 1
+# %B=full name of month in words..................(Uletter) 0
+# %Y=full numeric year.............................(Number) 1  -> {0101}
+print("Example 2:", s1)
 
 s2 = strftime("%c")
-print("Example 3: ", s2)
+# I used the %D short / date as my reference and dumped the HMS time in between day and year
+print("Example 3:", s2)
 
-# Please note MOTY: month of the year
-# please note DOTY: Day of the year
-# Simple representation
-s3 = strftime(" %R ")
-print("Example 3: ", s3)
+s3 = strftime("%R")
+# prints the 24h time format no seconds
+print("Example 4:", s3)
+
+s4 = strftime("%r")
+# prints the 12h time format
+print("Example 4:", s4)
+
+s5 = strftime("%a, %d %b %Y %I:%M:%S + 0000", gmtime())
+# %I=changes the time format to 12h.
+print("Example 5:", s5)
+
+
+s6 = strftime("%r, %T ", gmtime())
+# %T=current time == %H:%M:%S
+print("Example 6:", s6)
+
+s7 = strftime("%x, %X, %y, %Y")
+# %x=day starts short / date all numbers
+# %X=24h time with seconds included
+# %y=the year youngin'
+# %Y=full yer
+print("Example 7:", s7)
+
+s8 = strftime("%r, %z, %Z")
+# %r=12 h format with seconds
+# %z=time zone in GMT
+# %Z=time zone in West African...
+print("Example 8:", s8)
